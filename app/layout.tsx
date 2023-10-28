4e1b33a513c5bf5fc15c17ee5bd39abf48ebeb88
yunusoruk <orukyunus@gmail.com>
2023-10-28 16:00:25 +0300

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,68 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toaster-provider'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { siteConfig } from '@/config/site'
+import { Metadata } from 'next'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "Near",
+      url: "https://near.io",
+    },
+  ],
+  creator: "near",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "tr_TR",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: "@near",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 }
+
 
 export default function RootLayout({
   children,
@@ -22,7 +76,7 @@ export default function RootLayout({
 
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "near",
+  description: "Near | Near Admin.",
+  url: "https://near.io",
+  ogImage: "https://near.io/og.jpg",
+  links: {
+    twitter: "https://twitter.com/near",
+    github: "https://github.com/yunusoruk",
+    mail: "mailto:info@near.io",
+  },
+};
